cb1cf165b57d5324fd5f8608247f940c617ce8f9
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-4.local>
2020-01-06 16:39:53 -0500

    new commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  baseUrl: 'https://seenit-backend.herokuapp.com'
+  production: false,
+  baseUrl: 'http://localhost:8080'
 };
 
 /*
